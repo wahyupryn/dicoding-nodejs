c20efb0723992c5510c92db691dccb351596d669
Wahyu Priyono <wahyupriyono37@gmail.com>
2023-11-04 08:59:35 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const coffee = require('./lib/coffee');
+const user = require('./user');
+
+
+const {name, price} = require('./lib/coffee');
+
+console.log("name", name);
+console.log("price", price);
+
+console.log(coffee);
+console.log(user);
---
+++ user.js
@@ -0,0 +1,8 @@
+// const http = require('http');
+const firstName = 'Harry';
+const lastName = 'Potter';
+ 
+ 
+/* gunakan object literal
+untuk mengekspor lebih dari satu nilai. */
+module.exports = { firstName, lastName };
