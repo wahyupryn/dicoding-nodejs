da08eff286c4ed27415ee5704b778466bc3fee02
Wahyu Priyono <wahyupriyono37@gmail.com>
2023-11-05 00:24:22 +0700

    Filesystem

--- Filesystem/index.js
+++ Filesystem/index.js
@@ -1,16 +1,16 @@
 const fs = require('fs');
-// const { resolve } = require('path');
+const { resolve } = require('path');
      
-const data = fs.readFileSync('notes.txt', 'UTF-8');
-console.log(data);
+// const data = fs.readFileSync('notes.txt', 'UTF-8');
+// console.log(data);
 
 /* Method ini menerima tiga argumen yakni: lokasi berkas, encoding, dan callback function yang akan terpanggil bila berkas berhasil/gagal diakses */
-// const fileReadCallback = (error, data) => {
-//   if(error) {
-//     console.log('Gagal membaca berkas');
-//     return;
-//   }
-//   console.log(data);
-// };
+const fileReadCallback = (error, data) => {
+   if(error) {
+     console.log('Gagal membaca berkas');
+     return;
+   }
+   console.log(data);
+ };
 
-// fs.readFile(resolve(__dirname, 'notes.txt'), 'UTF-8', fileReadCallback);
+fs.readFile(resolve(__dirname, 'notes.txt'), 'UTF-8', fileReadCallback);
